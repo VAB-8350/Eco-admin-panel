669a0b6f78979ba0c59adbcebc53fe0b85ca782d
Andres Barilin <victor.barilin@gmail.com>
2025-07-16 14:33:52 -0300

    Use dark color for white page

--- src/components/MyComponents/ErrorMessage.jsx
+++ src/components/MyComponents/ErrorMessage.jsx
@@ -5,7 +5,7 @@ export default function ErrorMessage({ message }) {
     <div className='w-full flex flex-col items-center justify-center gap-2.5 mt-10'>
       <Ban className='w-10 h-10' />
       <h3 className='text-xl'>Ups! Hubo un error al cargar los datos...</h3>
-      <span className='text-red-500 text-sm'>{message}</span>
+      <span className='dark:text-red-500 text-red-600 text-sm'>{message}</span>
     </div>
   )
 }
