cf8de4bda628289738116a7e500f77235af770f9
Andres Barilin <victor.barilin@gmail.com>
2025-07-30 11:44:49 -0300

    fix schema

--- src/Screens/AddClient/ClientForm/schema.js
+++ src/Screens/AddClient/ClientForm/schema.js
@@ -1,7 +1,8 @@
 import { z } from 'zod'
 
 export const formSchema = z.object({
-  name: z.string().min(10, 'Este campo es requerido.'),
-  lastName: z.string().min(10, 'Este campo es requerido.'),
-  DNI: z.string().min(10, 'Este campo es requerido.'),
+  name: z.string().min(2, 'Este campo es requerido.'),
+  lastName: z.string().min(2, 'Este campo es requerido.'),
+  DNI: z.string().min(8, 'Este campo es requerido.'),
+  note: z.string()
 })
