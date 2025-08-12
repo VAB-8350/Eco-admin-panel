f53b8675111f0acc1b41dd4f45d698cf6adf2b16
Andres Barilin <victor.barilin@gmail.com>
2025-08-12 16:09:00 -0300

    Add submit btn

--- src/Screens/EditClient/EditClient.jsx
+++ src/Screens/EditClient/EditClient.jsx
@@ -5,6 +5,8 @@ import { Dialog } from '@/components/ui/dialog'
 import useAxiosPrivate from '@/hooks/useAxiosPrivate'
 import { useQuery } from '@tanstack/react-query'
 import { useParams } from 'react-router-dom'
+import { Button } from '@/components/ui/button'
+import { Save, X } from 'lucide-react'
 
 export default function EditClients() {
   
@@ -66,11 +68,29 @@ export default function EditClients() {
     }
   }, [data])
 
+  const goBack = () => {
+    const res = confirm('¿Estás seguro que deseas salir? Se perderán los cambios no guardados.')
+    res && window.history.back()
+  }
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <main className='h-[calc(100vh-180px)]'>
         <header className='flex justify-between flex-row gap-5 h-[10%] lg:h-[15%]'>
           <h1 className='text-4xl font-bold'>Editar Cliente</h1>
+
+          <div className='flex gap-3'>
+            <Button type='button' className='font-bold w-fit text-md hover:cursor-pointer' form='client-form' variant='outline' asChild>
+              <button onClick={goBack} className='flex items-center gap-2'>
+                <X />
+                Cancelar
+              </button>
+            </Button>
+            <Button type='submit' className='font-bold w-fit text-md hover:cursor-pointer' form='client-form'>
+              <Save />
+              Guardar cliente
+            </Button>
+          </div>
         </header>
 
         <ScrollArea className='flex flex-col w-full mx-auto overflow-hidden h-[90%]'>
