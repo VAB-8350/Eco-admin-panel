497a3b368a124293931066f3d3121016997cd76a
Andres Barilin <victor.barilin@gmail.com>
2025-08-12 23:56:44 -0300

    add try catch

--- src/components/MyComponents/ClientForm/useClientQueries.jsx
+++ src/components/MyComponents/ClientForm/useClientQueries.jsx
@@ -10,51 +10,83 @@ export default function useClientQueries() {
   //* Client
   const createClientMutation = useMutation({
     mutationFn: async (data) => {
-      return await axiosPrivate.post('/customers', data)
+      try {
+        return await axiosPrivate.post('/customers', data)
+      } catch {
+        return false
+      }
     },
   })
 
   const modifyClientMutation = useMutation({
     mutationFn: async (clientId, data) => {
-      return await axiosPrivate.patch(`/customers/${clientId}`, data)
+      try {
+        return await axiosPrivate.patch(`/customers/${clientId}`, data)
+      } catch {
+        return false
+      }
     },
   })
 
   //* Address
   const createAddressMutation = useMutation({
     mutationFn: async (clientId, data) => {
-      return await axiosPrivate.post(`/customers/${clientId}/addresses`, data)
+      try {
+        return await axiosPrivate.post(`/customers/${clientId}/addresses`, data)
+      } catch {
+        return false
+      }
     },
   })
 
   const modifyAddressMutation = useMutation({
     mutationFn: async (clientId, addressId, data) => {
-      return await axiosPrivate.patch(`/customers/${clientId}/addresses/${addressId}`, data)
+      try {
+        return await axiosPrivate.patch(`/customers/${clientId}/addresses/${addressId}`, data)
+      } catch {
+        return false
+      }
     },
   })
 
   const deleteAddressMutation = useMutation({
     mutationFn: async (clientId, addressId) => {
-      return await axiosPrivate.delete(`/customers/${clientId}/addresses/${addressId}`)
+      try {
+        return await axiosPrivate.delete(`/customers/${clientId}/addresses/${addressId}`)
+      } catch {
+        return false
+      }
     },
   })
 
   //* Contact
   const createContactMutation = useMutation({
     mutationFn: async (clientId, data) => {
-      return await axiosPrivate.post(`/customers/${clientId}/contacts`, data)
+      try {
+        return await axiosPrivate.post(`/customers/${clientId}/contacts`, data)
+      } catch {
+        return false
+      }
     },
   })
 
   const modifyContactMutation = useMutation({
     mutationFn: async (clientId, contactId, data) => {
-      return await axiosPrivate.patch(`/customers/${clientId}/contacts/${contactId}`, data)
+      try {
+        return await axiosPrivate.patch(`/customers/${clientId}/contacts/${contactId}`, data)
+      } catch {
+        return false
+      }
     },
   })
 
   const deleteContactMutation = useMutation({
     mutationFn: async (clientId, contactId) => {
-      return await axiosPrivate.delete(`/customers/${clientId}/contacts/${contactId}`)
+      try {
+        return await axiosPrivate.delete(`/customers/${clientId}/contacts/${contactId}`)
+      } catch {
+        return false
+      }
     },
   })
 
